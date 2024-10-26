257efa60cc79cf5090dbaf18842cd9e1fe5a9ebd
ImMab08 <frankyvargas15@gmail.com>
2024-10-26 16:16:59 -0500

    v0.1

---
+++ src/test/java/com/example/lesaccesorios/LesaccesoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesaccesorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesaccesoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
